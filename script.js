31205cdf1aa469aa8e459bc00ea4561ac127a6ae
Vera Lim <xdoki26@gmail.com>
2020-03-31 17:55:17 +0800

    Updates
    - Add option to select X or O
    - Reload game when it's Draw.

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-window.onload = function() {
-	// alert('Choose a player');
-}
+$(document).ready(function(){
+// Show the Modal on load
+	$("#myModal").modal("show");
+		
+	// Hide the Modal
+	$(".btn").click(function(){
+		$("#myModal").modal("hide");
+	});
+});
 
-var global_count = 1
+var global_count = 1;
 var squares = Array(9).fill(null);
 var win = false;
+var player1, player2 = "";
+
+function choosePlayer(i) {
+	if (i == 1){
+		player1 = "❌";
+		player2 = "⭕";
+	}
+	else {
+		player2 = "❌";
+		player1 = "⭕";
+	}
+}
 
 function handleClick(i) {
 	cell_id = 'c' + i;
 	var cell = document.getElementById(cell_id);
 	if(cell.innerHTML == "") {
 		if(global_count % 2 == 0){
-			cell.innerHTML = squares[i]  = "⭕";
+			cell.innerHTML = squares[i]  = player2;
 		} else {
-			cell.innerHTML = squares[i]  = "❌";
+			cell.innerHTML = squares[i]  = player1;
 		}
 		global_count += 1;
 	}
 	winner = calculateWinner(squares);
+
 	if (winner !== null && win == false) {
 		alert(winner + " wins!");
 		win = true;
+		global_count = 0;
+		location.reload(true);
+	}
+	if(global_count > 9){
+		alert("Draw!");
+		win = false;
+		global_count = 0;
 		location.reload(true);
 	}
 }
